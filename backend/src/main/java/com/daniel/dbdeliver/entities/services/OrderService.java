60e9faa3ff061239e4aff4071a6c78780e1387fb
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-18 15:05:02 -0300

    Post /order

--- backend/src/main/java/com/daniel/dbdeliver/entities/services/OrderService.java
+++ backend/src/main/java/com/daniel/dbdeliver/entities/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.daniel.dbdeliver.entities.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,19 +9,44 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.daniel.dbdeliver.entities.Order;
+import com.daniel.dbdeliver.entities.Product;
 import com.daniel.dbdeliver.entities.dto.OrderDTO;
+import com.daniel.dbdeliver.entities.dto.ProductDTO;
+import com.daniel.dbdeliver.entities.enums.OrderStatus;
 import com.daniel.dbdeliver.entities.repositories.OrderRepository;
+import com.daniel.dbdeliver.entities.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Transactional(readOnly=true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findAllWithproducts();
 		return list.stream().map(x -> new OrderDTO(x, x.getProducts())).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order entity = new Order();
+		entity.setAddress(dto.getAddress());
+		entity.setLatitude(dto.getLatitude());
+		entity.setLongitutde(dto.getLongitude());
+		entity.setMoment(Instant.now());
+		entity.setStatus(OrderStatus.PENDING);
+		
+		for(ProductDTO id: dto.getListProductDTO()) {
+			Product productId = productRepository.getById(id.getId());
+			entity.getProducts().add(productId);
+		}
+		
+	    entity = repository.save(entity);
+	    return new OrderDTO(entity);
 	}
 
 }
