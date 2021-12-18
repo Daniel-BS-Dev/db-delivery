2a6efabd3272a12eea02f61a562e996563988655
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-18 10:25:18 -0300

    Backend Created

---
+++ backend/src/main/java/com/daniel/dbdeliver/DbdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.dbdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/daniel/dbdeliver/DbdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.dbdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
