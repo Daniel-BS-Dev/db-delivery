d74338d9b1cbc9de8a7c13a6d3c2c0c1d84a8384
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-21 15:21:15 -0300

    Format

--- front-web/src/components/ProductCard/index.tsx
+++ front-web/src/components/ProductCard/index.tsx
@@ -5,12 +5,21 @@ type Props = {
     product: Product;
 }
 
+function formatPrice(price: number){
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+
+    return formatter.format(price);
+}
+
 const ProductCard = ({product}:Props) => {
     return (
       <div className="card-container">
          <h4 className="card-title">{product.name}</h4>
          <img src={product.imageUri} alt={product.name} className="card-image"/>
-         <h3 className="card-price"> R$ {product.price}</h3>
+         <h3 className="card-price"> {formatPrice(product.price)}</h3>
          <div className="card-description">
             <h4 className="description-title">Descrição</h4>
             <p className="description">{product.description}</p>
