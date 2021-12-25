deb3ead55e080902b23371ccdf2113107e3c8b66
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-25 15:58:35 -0300

    orderDetails

--- front-mobile/src/pages/OrderDetails/index.tsx
+++ front-mobile/src/pages/OrderDetails/index.tsx
@@ -1,7 +1,9 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import React from 'react';
 import Header from '../../components/Header';
 import { Order } from '../../types';
+import OrderCard from '../../components/OrderCard';
+import { RectButton } from 'react-native-gesture-handler';
 //pegando uma order que esta atrelada a uma rota
 type Props = {
   route : {
@@ -17,11 +19,49 @@ const OrderDetails = ({ route } : Props) => {
     return(
         <>
           <Header />
-          <View>
-            <Text>{order.id}</Text>
+          <View style={styles.container}> 
+            <OrderCard order={order} />
+            <RectButton style={styles.containerButton}>
+              <Text style={styles.textButton}>INICIAR NAVEGAÇÃO</Text>
+            </RectButton>
+            <RectButton style={styles.containerButton}>
+              <Text style={styles.textButton}>CONFIRMAR ENTREGA</Text>
+            </RectButton>
+            <RectButton style={styles.containerButton}>
+              <Text style={styles.textButton}>CANCELAR</Text>
+            </RectButton>
           </View>
         </>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingRight: '5%',
+    paddingLeft: '5%'
+  },
+
+  containerButton: {
+    backgroundColor: '#DA5C5C',
+    flexDirection: 'row',
+    borderRadius: 10,
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  textButton: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#FFF',
+    letterSpacing: -0.24,
+    fontFamily: 'OpenSans_700Bold'
+  }
+
+});
 
 export default OrderDetails;
